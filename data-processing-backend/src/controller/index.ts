be712f8bd2fc8e0ea069b1c8f964d94f97ffed06
Mihaela Covali <mihaelacovali@Mihaelas-MacBook-Pro.local>
2025-01-12 20:33:23 +0100

    Fixing the errors

--- data-processing-backend/src/controller/index.ts
+++
@@ -1,7 +0,0 @@
-import { Request, Response } from 'express';
-import responder from '../utils/responder';
-
-export const getIndexPage = async (req: Request, res: Response): Promise<void> => {
-
-  responder(res, 200, req.headers['content-type']!, "2", "2")
-};
